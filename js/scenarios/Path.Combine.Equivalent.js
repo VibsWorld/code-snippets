4e0044330ec5599f0face2b3e8f8a335ba158495
vibs2006 <vibs2006@gmail.com>
2025-10-25 12:52:19 +0530

    Implement pathJoin function for joining paths

---
+++ js/scenarios/Path.Combine.Equivalent.js
@@ -0,0 +1,15 @@
+//Path.Combine Equivalent in Javascript
+
+function pathJoin(parts, sep){
+    const separator = sep || '/';
+    parts = parts.map((part, index)=>{
+        if (index) {
+            part = part.replace(new RegExp('^' + separator), '');
+     		}
+        if (index !== parts.length - 1) {
+            part = part.replace(new RegExp(separator + '$'), '');
+        }
+        return part;
+    })
+    return parts.join(separator);
+ }
